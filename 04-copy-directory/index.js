d7abd3318d73f9aad79a76aa77dde011cfbe25c5
Artem.Dubovov <artem.dubovov@bk.ru>
2022-05-19 14:30:16 +0300

    feat: finish 3 and 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+
+async function copyDir(){
+
+  const dir = path.join(__dirname, 'files');
+  const dirCopy = path.join(__dirname, 'files-copy');
+
+  await fs.readdir(dir, {withFileTypes: false, encoding: 'utf-8'}, (err, files) => {
+    if(err) throw err;
+
+    // create folder
+    fs.promises.mkdir(dirCopy, {recursive : true}, error => {
+      if(error) throw error;
+    });
+
+
+    for (let file of files){
+      const filename = path.parse(file).base;
+      let stream = fs.createReadStream(path.join(dir, filename));
+      let output = fs.createWriteStream(path.join(dirCopy, filename));
+      stream.pipe(output);
+    }
+
+  });
+}
+
+copyDir();
 
