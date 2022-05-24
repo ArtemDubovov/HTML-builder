350ff7e7548dc5da827056393db2d2397cbc9e6a
Artem.Dubovov <artem.dubovov@bk.ru>
2022-05-24 16:09:07 +0300

    fix: fix 5 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,56 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-// // function deleted old files
 
-// async function deleted(pathItems, faceFiles){
-//   try{
-//     for await(let item of faceFiles){
-//       fs.unlink(path.join(pathItems, item), err => {
-//         if(err) throw err;
-//       });
-//     }
-//   } catch (e){
-//     process.stdout.write(e);
-//   }
-// }
-
-// async function copyDir(){
-//   try{
-//     const dir = path.join(__dirname, 'files');
-//     const dirCopy = path.join(__dirname, 'files-copy');
-
-//     // Create folder
-
-//     await fs.mkdir(dirCopy, {recursive : true}, error => {
-//       if(error) throw error;
-//     });
-
-//     // Delete files-copy
-
-//     await fs.readdir(dirCopy, {withFileTypes: false,}, (err, faceFiles) => {
-//       if(err) throw err;
-//       deleted(dirCopy, faceFiles);
-//     });
-
-//     // Copy-files
-
-//     await fs.readdir(dir, {withFileTypes: false,}, (err, files) => {
-//       if(err) throw err;
-//       for (let file of files){
-//         const filename = path.parse(file).base;
-//         let stream = fs.createReadStream(path.join(dir, filename));
-//         let output = fs.createWriteStream(path.join(dirCopy, filename));
-//         stream.pipe(output);
-//       }
-
-//     });
-//   } catch (e){
-//     process.stdout.write(e);
-//   }
-// }
-
-// copyDir();
 
 function copyDir(dir, dirCopy){
   fs.readdir(dir, {withFileTypes: false,}, (err, files) => {
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,50 +4,29 @@ const path = require('path');
 const dirOutput = path.join(__dirname, 'styles');
 const dirInput = path.join(__dirname, 'project-dist');
 
-async function copyFiles(dir, files){
-  try {
-    for await (let item of files){
-      if(item.isFile() && path.extname(item.name) == '.css'){
-        let wrapper = '';
-        const inp = fs.createReadStream(path.join(dirOutput, item.name), 'utf-8');
-        inp.on('data', chunk => wrapper += chunk);
-        inp.on('end', () => {
-          fs.appendFile(path.join(dirInput, 'bundle.css'), wrapper, err => {
-            if(err) throw err;
-          });
-        });
-      }
-    }
-  } catch (e){
-    process.stdout.write(e);
-  }
-}
+function copyFiles(dir, files){
 
-async function start(){
-  try { 
-    // создать файл пустой
-    await fs.open(path.join(dirInput, 'bundle.css'), 'w', err => {
-      if(err) throw err;
-      fs.writeFile(path.join(dirInput, 'bundle.css'), '', err => {
-        if(err) throw err;
-      });
-    });
-    // прочитать папку
-    await fs.readdir(dirOutput, {withFileTypes: true}, (err, files) => {
-      (async () => {
-        try{
+  files.forEach(item => {
+    if(item.isFile() && path.extname(item.name) == '.css'){
+      let wrapper = '';
+      const inp = fs.createReadStream(path.join(dirOutput, item.name), 'utf-8');
+      inp.on('data', chunk => wrapper += chunk);
+      inp.on('end', () => {
+        fs.appendFile(path.join(dirInput, 'bundle.css'), wrapper, err => {
           if(err) throw err;
-          await copyFiles(dirOutput, files);
-        } catch (e){
-          process.stdout.write(e);
-        }
-      })();
-      
-    });
-
-    // скопировать папаку
-  } catch (e) {
-    process.stdout.write(e);
-  }
+        });
+      });
+    }
+  });
 }
-start();
+
+fs.open(path.join(dirInput, 'bundle.css'), 'w', err => {
+  if(err) console.log(err);
+  fs.writeFile(path.join(dirInput, 'bundle.css'), '', err => {
+    if(err) console.log(err);
+    fs.readdir(dirOutput, {withFileTypes: true}, (err, files) => {
+      if(err) console.log(err);
+      copyFiles(dirOutput, files);
+    });
+  });
+});
